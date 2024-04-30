4a69bbdec58f4cafd0952a071687d55d88d916ae
Berrym-tech <71224774+Berrym-tech@users.noreply.github.com>
2024-04-29 20:56:10 -0400

    Update Decryption.py

--- Decryption.py
+++ Decryption.py
@@ -5,7 +5,6 @@ from cryptography.fernet import Fernet
 def load_key():
     with open('Ransom.key', 'rb') as filekey:
         key = filekey.read()
-    return key
 
 # Function to decrypt files
 def decrypt_file(encrypted_file_path, key):
@@ -21,7 +20,6 @@ encrypted_directory_path = "/home/maxwell/Desktop/Happy Files"
 
 # Load the encryption key
 key = load_key()
-print("Encryption Key:", key)  # Debugging statement
 
 # Decrypt files with the .encrypted extension
 for root, dirs, files in os.walk(encrypted_directory_path):
